3847834f2fbfe3406e08ff1f73f5d76e37a8b5fb
alex.nguyen <alex.nguyen@applifting.cz>
2021-10-23 19:30:47 +0200

    hovna

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,27 +1,34 @@
-import React, { useState, useEffect } from "react";
-import Quote from "./Quote";
-import Diary from "./Diary";
+import React, { useState, useEffect } from 'react';
+import Quote from './Quote';
+import Diary from './Diary';
 
-const Home: React.FC = () => {
-  const [loading, setLoading] = useState<boolean>(false);
+const Home: React.FC = ({ children }) => {
+  // const [loading, setLoading] = useState<boolean>(false);
+  const [form2, setForm2] = useState({ jmeno: 'alex' });
 
-  useEffect(() => {
-    setLoading(true);
-    let timer = setTimeout(() => {
-      setLoading(false);
-    }, 200);
-    return () => clearTimeout(timer);
-  }, []);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   let timer = setTimeout(() => {
+  //     setLoading(false);
+  //   }, 200);
+  //   return () => clearTimeout(timer);
+  // }, []);
+
+  const formData = { jmeno: 'alex' };
+
+  const handleClick = () => {
+    // formData.jmeno = 'silva';
+    setForm2({ jmeno: 'silva' });
+    console.log(formData);
+  };
+
+  console.log('rendered');
+  console.log(formData);
 
   return (
     <div className="layout">
-      <nav>Logo</nav>
-      <main>
-        <h1>Home</h1>
-        <Quote />
-        <Diary loading={loading} />
-      </main>
-      <footer>Footer @applifting2020</footer>
+      <button onClick={handleClick}>hello there</button>
+      {form2.jmeno}
     </div>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    chunkFilename: "[id].js",
-    publicPath: "",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    chunkFilename: '[id].js',
+    publicPath: '',
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx|tsx|ts)$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
     ],
   },
   // includes the webpack bundle into the index.html
   plugins: [
     new HtmlWebpackPlugin({
-      template: __dirname + "/src/public/index.html",
-      filename: "index.html",
-      inject: "body",
+      template: path.join(__dirname, '/src/public/index.html'),
+      filename: 'index.html',
+      inject: 'body',
     }),
   ],
   // dev server options
