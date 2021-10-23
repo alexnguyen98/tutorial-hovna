0a5bd90194dce524ac98dcbd01f8f2b1677624a9
alex.nguyen <alex.nguyen@applifting.cz>
2021-10-23 20:29:42 +0200

    update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,15 +3,32 @@ import Quote from './Quote';
 import Diary from './Diary';
 
 const Home: React.FC = ({ children }) => {
-  const jmeno = 'alex';
-  const handleClick = () => {};
+  const [state, setState] = useState<string>('samuel');
+
+  const handleClick = () => {
+    setState('alex');
+  };
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
 
   return (
     <div className="layout">
       <button onClick={handleClick}>hello there</button>
-      {jmeno}
+      {state}
+      <Hovno hovno={[]} />
     </div>
   );
+};
+
+type Props = {
+  hovno: any;
+};
+
+const Hovno: React.FC<Props> = ({ hovno }) => {
+  console.log('k');
+  return <div>Hovno</div>;
 };
 
 export default Home;
