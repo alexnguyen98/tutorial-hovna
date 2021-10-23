18538a2b8480c95b353dee3070d3022df2d7386e
alex.nguyen <alex.nguyen@applifting.cz>
2021-10-23 19:51:54 +0200

    update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,32 +3,13 @@ import Quote from './Quote';
 import Diary from './Diary';
 
 const Home: React.FC = ({ children }) => {
-  // const [loading, setLoading] = useState<boolean>(false);
-  const [form2, setForm2] = useState({ jmeno: 'alex' });
-
-  // useEffect(() => {
-  //   setLoading(true);
-  //   let timer = setTimeout(() => {
-  //     setLoading(false);
-  //   }, 200);
-  //   return () => clearTimeout(timer);
-  // }, []);
-
-  const formData = { jmeno: 'alex' };
-
-  const handleClick = () => {
-    // formData.jmeno = 'silva';
-    setForm2({ jmeno: 'silva' });
-    console.log(formData);
-  };
-
-  console.log('rendered');
-  console.log(formData);
+  const jmeno = 'alex';
+  const handleClick = () => {};
 
   return (
     <div className="layout">
       <button onClick={handleClick}>hello there</button>
-      {form2.jmeno}
+      {jmeno}
     </div>
   );
 };
